923b3aa5e065a408b1f9db269985dce83a975e13
WH <chuawh.74@gmail.com>
2025-07-12 20:14:56 +0800

    Delete assets directory

--- assets/BLrqIqwk-DLcrDxML.js
+++
@@ -1,1 +0,0 @@
-import{r as t,d as c}from"./index-QSsAJGx7.js";const o=({label:a="No channels",...e})=>t.createElement(c,{icon:"chat",label:a,...e});export{o as default};
--- assets/DnKaqbmp-BXNTQ1zY.js
+++
@@ -1,1 +0,0 @@
-import{r as s,w as t}from"./index-QSsAJGx7.js";const c=({iconSize:e=48,className:a})=>s.createElement(t,{size:e,className:a});export{c as default};
